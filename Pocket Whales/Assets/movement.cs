d1a33ca27a826ad0e6b918989ab4487d78f7332c
camronvick <camronvick@aol.com>
2018-02-01 20:42:30 -0600

    Flipping Whale
    Our First Script

---
+++ Pocket Whales/Assets/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Rotate the object around its local X axis at 1 degree per second
+		transform.Rotate(Vector3.back * 30*(Time.deltaTime));
+
+		// ...also rotate around the World's Y axis
+		//transform.Rotate(Vector3.up * (20 * Time.deltaTime), Space.World);
+	}
+}
