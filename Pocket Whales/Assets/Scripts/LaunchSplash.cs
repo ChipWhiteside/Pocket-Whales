df434bc17bb0d600ece1f79945bb2242c607ac47
camronvick <camronvick@aol.com>
2018-02-13 01:45:13 -0600

    Cam's Prototype 1
    Added the Gameplay scene. Each player has one chance to move a fixed distance (arrow keys) and fire (space bar) on their turn. Once they have fired, control moves to the other player. Projectile power and angle of launch can be changed with the force and angle fields of the player controller script.

---
+++ Pocket Whales/Assets/Scripts/LaunchSplash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchSplash : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+
+	// Use this for initialization
+	public void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.IsSleeping()) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void Launch() {
+		
+	}
+
+	void FixedUpdate ()
+	{
+		
+	}
+}
