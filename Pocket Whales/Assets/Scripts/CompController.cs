510cd46a9304d416baf7410a7fef681919e5b451
ChipWhiteside <chip.whiteside@my.wheaton.edu>
2018-03-03 11:15:22 -0600

    Committing

--- Pocket Whales/Assets/Scripts/CompController.cs
+++ Pocket Whales/Assets/Scripts/CompController.cs
@@ -142,6 +142,48 @@ public class CompController : MonoBehaviour {
 		sr.sprite = whaleIdle;
 	}
 
+	void FireProjectile() {
+		Transform target;
+
+		float initialAngle;
+		Vector3 pos = new Vector3 (0, 1, 0); //position of the computer whale
+		GameObject splash = Instantiate (projectile, transform.position + pos, transform.rotation); //projectile gets same position and rotation as whale
+		splash.SetActive (true);
+		Rigidbody2D splashrb = splash.GetComponent<Rigidbody2D> ();
+
+			var rigid = GetComponent<Rigidbody>();
+
+			Vector3 p = target.position;
+
+			float gravity = Physics.gravity.magnitude;
+			// Selected angle in radians
+			float angle = initialAngle * Mathf.Deg2Rad;
+
+			// Positions of this object and the target on the same plane
+			Vector3 planarTarget = new Vector3(p.x, 0, p.z);
+			Vector3 planarPostion = new Vector3(transform.position.x, 0, transform.position.z);
+
+			// Planar distance between objects
+			float distance = Vector3.Distance(planarTarget, planarPostion);
+			// Distance along the y axis between objects
+			float yOffset = transform.position.y - p.y;
+
+			float initialVelocity = (1 / Mathf.Cos(angle)) * Mathf.Sqrt((0.5f * gravity * Mathf.Pow(distance, 2)) / (distance * Mathf.Tan(angle) + yOffset));
+
+			Vector3 velocity = new Vector3(0, initialVelocity * Mathf.Sin(angle), initialVelocity * Mathf.Cos(angle));
+
+			// Rotate our velocity to match the direction between the two objects
+			float angleBetweenObjects = Vector3.Angle(Vector3.forward, planarTarget - planarPostion);
+			Vector3 finalVelocity = Quaternion.AngleAxis(angleBetweenObjects, Vector3.up) * velocity;
+
+			// Fire!
+		splashrb.velocity = finalVelocity;
+
+			// Alternative way:
+			// rigid.AddForce(finalVelocity * rigid.mass, ForceMode.Impulse);
+
+	}
+
 
 
 	/*void OnTriggerEnter(Collider other)
