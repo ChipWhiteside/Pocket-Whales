504f0e40333c92c575e1345c7ced51941ff9157d
ChipWhiteside <chip.whiteside@my.wheaton.edu>
2018-04-03 16:10:27 -0500

    Updates to clouds
    The game wont load when I press play now. I believe it is a memory issue. Pushing to get help debuging from teammates

--- Pocket Whales/Assets/Scripts/CloudManager.cs
+++ Pocket Whales/Assets/Scripts/CloudManager.cs
@@ -6,12 +6,16 @@ public class CloudManager : MonoBehaviour {
 
 	public int cloudsAllowed; //number of clouds allowed on screen
 
+	public float time; //amount of time we want to wait between cloud spawns
+	private float timeStore; 
+
 	private int numClouds; //number of clouds in the game
 
 	public GameObject originCloud; //the origional cloud
 
 	void Start () {
 		numClouds = 0;
+		timeStore = time;
 	}
 
 	/*
@@ -23,7 +27,26 @@ public class CloudManager : MonoBehaviour {
 	}
 
 	void Update () {
-		StartCoroutine (waitForTime (Random.Range(4f, 10f)));
+		if (time > 0) {
+			time -= Time.deltaTime;
+		} else {
+			print ("Kaitie is the cutest person eva");
+			//yield return new WaitForSeconds(7.0f); //check once every 7 seconds to see if the turn has ended
+			if (numClouds < cloudsAllowed) {
+				GameObject newCloud = Instantiate (originCloud);
+				Cloud newCloudScript = GameObject.Find("newCloud").GetComponent<Cloud>();
+				float speed = Random.Range (.7f, 1.2f);
+				float scale = Random.Range (.3f, .6f);
+				float height = Random.Range (10f, 27f);
+				newCloudScript.setValues (speed, scale, height);
+				//Cloud newCloud = new Cloud (speed, scale, height);
+			}	
+			time = timeStore;
+		}
+
+		/*
+		//StartCoroutine (waitForTime (Random.Range(4f, 10f)));
+		yield return new WaitForSeconds(10f);
 		print ("waiting for time");
 		//yield return new WaitForSeconds(7.0f); //check once every 7 seconds to see if the turn has ended
 		if (numClouds < cloudsAllowed) {
@@ -35,5 +58,6 @@ public class CloudManager : MonoBehaviour {
 			newCloudScript.setValues (speed, scale, height);
 			//Cloud newCloud = new Cloud (speed, scale, height);
 		}
+		*/
 	}
 }
