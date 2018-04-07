e017c076d6e234b71fe70e6d125acc0bb0b7863d
camronvick <camronvick@aol.com>
2018-04-07 10:21:22 -0500

    Bouncer Splash
    Added new splash

---
+++ Pocket Whales/Assets/Scripts/SplashScripts/BouncerScript.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncerScript : MonoBehaviour {
+
+	/*
+	 * Every active splash
+	 */
+	private GameObject[] allSplashes;
+
+	/*
+	 * Energy whale loses when hit by this splash 
+	 */
+	private int energyEffect;
+
+	/*
+	 * Time the splash can stay active 
+	 */
+	private float maxActiveTime;
+
+	/*
+	 * Time until special effect if this splash has one
+	 */
+	private float timeUntilEffect;
+
+	/*
+	 * Counter until special effect
+	 */
+	private float effectTimer;
+
+	/*
+	 * counts up so the splash eventually disappears 
+	 */
+	private float despawnTimer;
+
+	/*
+	 * Controller Game Object
+	 */
+	public GameObject control;
+
+	/*
+	 * ControlScript
+	 */
+	private ControlScript controlScript;
+
+	/*
+	 * Is the turn already ending
+	 */
+	private bool endingTurn;
+
+	/*
+	 * How many times has it bounced
+	 */
+	private int bounceCount;
+
+	private Vector2 firstBounceVelocityNormalized;
+
+	// Use this for initialization
+	void Start () {
+		energyEffect = 10;
+		maxActiveTime = 20;
+		despawnTimer = 0; //always starts at zero
+		effectTimer = 0; //always starts at zero
+		timeUntilEffect = 0; //when the special effect should happen
+		controlScript = control.GetComponent<ControlScript> ();
+		endingTurn = false;
+		bounceCount = 0;
+
+		EffectOnLaunch ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		despawnTimer += Time.deltaTime;
+		if (despawnTimer >= maxActiveTime)
+			EndTurn ();
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.gameObject.CompareTag("Terrain")) {
+			EffectOnBounce ();
+		}
+		if (collision.gameObject.CompareTag("Splash")) {
+			Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+		}
+		if (collision.gameObject.CompareTag("Whale")) {
+			EffectOnHit ();
+		}
+	}
+
+	void EffectOnLaunch() {
+
+	}
+
+	void EffectOnTime() {
+
+	}
+
+	void EffectOnHit() {
+		if (bounceCount == 0) {
+
+		}
+		
+		if(bounceCount >= 7){
+			EndTurn ();
+		}
+		bounceCount++;
+	}
+
+	void EffectOnBounce() {
+		
+		if(bounceCount >= 7){
+			EndTurn ();
+		}
+		bounceCount++;
+	}
+
+	void EndTurn() {
+		if (!endingTurn) { //so we only try to end the turn once
+			endingTurn = true;
+			Destroy (gameObject);
+			DestroyAllObjects ();
+			controlScript.SwitchPlayerControl ();
+		}
+	}
+
+	void DestroyAllObjects()
+	{
+		allSplashes = GameObject.FindGameObjectsWithTag ("Splash");
+		for(var i = 0 ; i < allSplashes.Length ; i++)
+		{
+			Destroy(allSplashes[i]);
+		}
+	}
+
+}
