5ce14691926864d45eb872b280b5de0ffafb2106
ChipWhiteside <chip.whiteside@my.wheaton.edu>
2018-04-07 10:01:58 -0500

    Clouds work perf

--- Pocket Whales/Assets/Scripts/CloudManager.cs
+++ Pocket Whales/Assets/Scripts/CloudManager.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 public class CloudManager : MonoBehaviour {
 
 	public float cloudsAllowed; //number of clouds allowed on screen
+	public float bCloudsAllowed; //number of background clouds allowed on screen
 
 	private float time; //amount of time we want to wait between cloud spawns
 	private float timeStore; 
 
 	public int numClouds; //number of clouds in the game
+	public int numBClouds; //number of background clouds on screen
 	private Vector3 cloudStart;
+	private Vector3 bCloudStart;
 
 	public GameObject originCloud; //the origional cloud
+	public GameObject backgroundOCloud; //the origional background cloud
 
 	void Start () {
 		time = Random.Range (4f, 18f);
 		numClouds = 0;
 		cloudsAllowed = Random.Range (7f, 20f);
-		print ("Clouds Allowed: " + cloudsAllowed);
-		timeStore = time;
-		cloudStart = new Vector3(60f, Random.Range (10f, 27f), 0f);
+		bCloudsAllowed = Random.Range (13f, 25f);
 
-		createCloud ();
-		createCloud ();
+		timeStore = time;
+		cloudStart = new Vector3(originCloud.transform.position.x, originCloud.transform.position.y, originCloud.transform.position.z);
+		bCloudStart = new Vector3(backgroundOCloud.transform.position.x, backgroundOCloud.transform.position.y, backgroundOCloud.transform.position.z);
+
+		int numCloudStart = (int) Random.Range (2f, 4f); //number of foregrounds clouds to be spawned at start
+		int numBCloudStart = (int) Random.Range (5f, 7f); //number of background clouds to be spawned at start
+
+		while (numCloudStart > 0) { //spawn a random number of foreground clouds at start of game
+			spawnCloud (Random.Range (-40f, 40f));
+			numCloudStart--;
+		}
+		while (numBCloudStart > 0) { //spawn random number of background clouds at start of game
+			spawnBCloud (Random.Range (-40f, 40f));
+			numBCloudStart--;
+		}
 	}
 
 	/*
@@ -36,6 +51,9 @@ public class CloudManager : MonoBehaviour {
 
 	void Update () {
 		originCloud.transform.position = cloudStart;
+		originCloud.transform.localScale = new Vector3(.4f, .4f, .4f);;
+		backgroundOCloud.transform.position = bCloudStart;
+		backgroundOCloud.transform.localScale = new Vector3(.2f, .2f, .2f);
 
 		if (time > 0) {
 			time -= (Time.deltaTime);
@@ -44,38 +62,26 @@ public class CloudManager : MonoBehaviour {
 			//print ("In else statment");
 			//yield return new WaitForSeconds(7.0f); //check once every 7 seconds to see if the turn has ended
 			if (numClouds < cloudsAllowed) {
-				Vector3 pos = new Vector3 (0, 1, 0);
-				GameObject newCloud = Instantiate(originCloud, cloudStart, originCloud.transform.rotation);
-				numClouds++;
-				CloudScript newCloudScript = newCloud.GetComponent<CloudScript> ();
-				newCloudScript.setValues (newCloud.transform.position.x, Random.Range (1f, 5f), Random.Range (.3f, .6f), Random.Range (12f, 30f));
-				//Cloud newCloud = new Cloud (speed, scale, height);
+				spawnCloud (originCloud.transform.position.x);
 			}	
+			if (numBClouds < bCloudsAllowed) {
+				spawnBCloud (backgroundOCloud.transform.position.x);
+			}
 		}
-
-
-
-		/*
-		//StartCoroutine (waitForTime (Random.Range(4f, 10f)));
-		yield return new WaitForSeconds(10f);
-		print ("waiting for time");
-		//yield return new WaitForSeconds(7.0f); //check once every 7 seconds to see if the turn has ended
-		if (numClouds < cloudsAllowed) {
-			GameObject newCloud = Instantiate (originCloud);
-			Cloud newCloudScript = GameObject.Find("newCloud").GetComponent<Cloud>();
-			float speed = Random.Range (.7f, 1.2f);
-			float scale = Random.Range (.3f, .6f);
-			float height = Random.Range (10f, 27f);
-			newCloudScript.setValues (speed, scale, height);
-			//Cloud newCloud = new Cloud (speed, scale, height);
-		}
-		*/
 	}
 
-	void createCloud () {
-		GameObject newCloud = Instantiate(originCloud, new Vector3(Random.Range(-40f, 40f), Random.Range(12f, 30f)), originCloud.transform.rotation);
+	void spawnCloud (float xVal) {
+		GameObject newCloud = Instantiate(originCloud, new Vector3(xVal, Random.Range(8.5f, 30f)), originCloud.transform.rotation);
 		numClouds++;
 		CloudScript newCloudScript = newCloud.GetComponent<CloudScript> ();
-		newCloudScript.setValues (newCloud.transform.position.x, Random.Range (1f, 5f), Random.Range (.3f, .6f), Random.Range (12f, 30f));
+		//newCloud (Sorting Layer: Piece) (Order in Layer: 1)
+		newCloudScript.setValues (newCloud.transform.position.x, Random.Range (2f, 5f), Random.Range (.45f, .6f), newCloud.transform.position.y);
+	}
+
+	void spawnBCloud(float xVal) {
+		GameObject newBCloud = Instantiate(backgroundOCloud, new Vector3(xVal, Random.Range(8.5f, 30f)), backgroundOCloud.transform.rotation);
+		numBClouds++;
+		CloudScript newBCloudScript = newBCloud.GetComponent<CloudScript> ();
+		newBCloudScript.setValues (newBCloud.transform.position.x, Random.Range (.5f, 1.5f), Random.Range (.15f, .25f), newBCloud.transform.position.y);
 	}
 }
